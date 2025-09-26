1977817a8e5040b83536ca7eadc80dce050b041b
lorenzo-f <lorenzo-f@users.noreply.github.com>
2025-09-26 11:10:42 +0000

    Fixed an issue with the Linux build in the Advanced Demo, and a sync bug with very fast CPUs lorenzo-f/WebGPU4Edu@fed7f0b103432b578f999bd344aefd2769b8796f

--- App.js
+++ App.js
@@ -29,7 +29,7 @@ var ENVIRONMENT_IS_SHELL = !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_NODE && !ENVIR
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /tmp/tmpin4r4daq.js
+// include: /tmp/tmp19d7wqzi.js
 Module["expectedDataFileDownloads"] ??= 0;
 
 Module["expectedDataFileDownloads"]++;
@@ -194,23 +194,23 @@ Module["expectedDataFileDownloads"]++;
   });
 })();
 
-// end include: /tmp/tmpin4r4daq.js
-// include: /tmp/tmpbenf83w3.js
+// end include: /tmp/tmp19d7wqzi.js
+// include: /tmp/tmpp2zjiy4i.js
 // All the pre-js content up to here must remain later on, we need to run
 // it.
 if ((typeof ENVIRONMENT_IS_WASM_WORKER != "undefined" && ENVIRONMENT_IS_WASM_WORKER) || (typeof ENVIRONMENT_IS_PTHREAD != "undefined" && ENVIRONMENT_IS_PTHREAD) || (typeof ENVIRONMENT_IS_AUDIO_WORKLET != "undefined" && ENVIRONMENT_IS_AUDIO_WORKLET)) Module["preRun"] = [];
 
 var necessaryPreJSTasks = Module["preRun"].slice();
 
-// end include: /tmp/tmpbenf83w3.js
-// include: /tmp/tmpiln3ak3w.js
+// end include: /tmp/tmpp2zjiy4i.js
+// include: /tmp/tmpl525e8vs.js
 if (!Module["preRun"]) throw "Module.preRun should exist because file support used it; did a pre-js delete it?";
 
 necessaryPreJSTasks.forEach(task => {
   if (Module["preRun"].indexOf(task) < 0) throw "All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?";
 });
 
-// end include: /tmp/tmpiln3ak3w.js
+// end include: /tmp/tmpl525e8vs.js
 var arguments_ = [];
 
 var thisProgram = "./this.program";
--- cmake_install.cmake
+++ cmake_install.cmake
@@ -2,7 +2,7 @@
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "/home/runner/work/_temp/115700c1-2e7a-4790-8a48-70c862820942/emsdk-main/upstream/emscripten/cache/sysroot")
+  set(CMAKE_INSTALL_PREFIX "/home/runner/work/_temp/79b49509-ffda-4df2-8fab-e3c3a58bcc0e/emsdk-main/upstream/emscripten/cache/sysroot")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
